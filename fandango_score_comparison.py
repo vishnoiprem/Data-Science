76e7cff01ba025cccdae9511ea55e9f8e5a0b799
vishnoiprem <vishnoiprem@gmail.com>
2018-02-14 22:05:57 +0800

    pandas

---
+++ fandango_score_comparison.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+import numpy as np 
+
+
+fandango_score_comparison=pd.read_csv("fandango_score_comparison.csv")
+
+
+print(fandango_score_comparison.columns)
+print(fandango_score_comparison.head(2))
